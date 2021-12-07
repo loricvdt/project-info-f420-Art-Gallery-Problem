5553d90e57bc5f3c9ea7f7a6cfb028c16a6fb80f
loricvdt <loricvdt@users.noreply.github.com>
2021-12-07 13:27:23 +0100

    Add about page, fix ray shooting, and add interactive canvas

--- common.js
+++ common.js
@@ -6,6 +6,7 @@ class Point {
 }
 
 const centre = new Point(300, 175)
+const farPoint = new Point(10000, 10000)
 const star = [
 	new Point(425, 175),
 	new Point(325, 150),
@@ -16,10 +17,14 @@ const star = [
 	new Point(300, 300),
 	new Point(325, 200),
 ]
+const savedPolygon = [new Point(156, 80), new Point(79, 172), new Point(150, 161), new Point(146, 262), new Point(84, 253), new Point(119, 299), new Point(180, 296), new Point(160, 263), new Point(168, 233), new Point(236, 229), new Point(210, 153), new Point(310, 192), new Point(281, 246), new Point(213, 284), new Point(284, 302), new Point(330, 215), new Point(351, 286), new Point(435, 169), new Point(514, 168), new Point(501, 39), new Point(367, 155), new Point(366, 83), new Point(452, 35), new Point(342, 13), new Point(330, 76), new Point(235, 61), new Point(178, 27),
+]
+
 
 function reset(p) {
-	p.points = []
+	resetGuards(p)
 	p.polygon = []
+	p.points = []
 }
 
 function resetGuards(p) {
@@ -31,22 +36,23 @@ function commonSetup(p, parent) {
 	let canvas = p.createCanvas(600, 350)
 	canvas.parent(parent)
 	p.strokeWeight(1)
+	return canvas
 }
 
 function commonDraw(p) {
 	p.background(200)
 
+	// Guards field of view
 	p.noStroke()
 	p.fill("orange")
-
 	const rayLines = []
 	for (const guard of p.guards) {
 		rayLines.push(...drawGuardView(p, guard))
 	}
 
+	// Guards line of sight
 	p.strokeWeight(0.5)
 	p.stroke("white")
-
 	for (const line of rayLines) {
 		p.line(line[0].x, line[0].y, line[1].x, line[1].y)
 	}
@@ -55,6 +61,7 @@ function commonDraw(p) {
 	p.stroke("black")
 	p.fill("black")
 
+	// Connected point list
 	let last;
 	for (const i in p.points) {
 		if (i >= 1) p.line(last.x, last.y, p.points[i].x, p.points[i].y)
@@ -62,6 +69,7 @@ function commonDraw(p) {
 		p.ellipse(p.points[i].x, p.points[i].y, 4, 4)
 	}
 
+	// Polygon
 	if (p.polygon.length > 0) {
 		last = p.polygon[p.polygon.length - 1];
 		for (const point of p.polygon) {
@@ -71,15 +79,16 @@ function commonDraw(p) {
 		}
 	}
 
+	// Guard positions
 	p.stroke("red")
 	p.fill("red")
-
 	for (const guard of p.guards) {
 		p.ellipse(guard.x, guard.y, 4, 4)
 	}
 }
 
 function drawGuardView(p, guard) {
+	// Find vertices in sight
 	const guardView = []
 	for(const i in p.polygon) {
 		guardView.push(Number(i))
@@ -93,15 +102,17 @@ function drawGuardView(p, guard) {
 		}
 	}
 
+	// Find correct intersects and draw guard view
 	const rayLines = []
 	p.beginShape()
 	for(const i in guardView) {
 		p.vertex(p.polygon[guardView[i]].x, p.polygon[guardView[i]].y)
+		// Find intersects if consecutive points are not an edge
 		if((guardView[i]+1) % p.polygon.length !== guardView[(Number(i)+1) % guardView.length]) {
-			const intersectSegment = findIntersect(p, guard, guardView[i], guardView[(Number(i)+1) % guardView.length])
-			if(intersectSegment) {
-				rayLines.push([intersectSegment[0], guard])
-				p.vertex(intersectSegment[0].x, intersectSegment[0].y)
+			const intersectSegments = findClosestIntersects(p, guard, guardView[i], guardView[(Number(i)+1) % guardView.length])
+			for(let segment of intersectSegments) {
+				rayLines.push([segment, guard])
+				p.vertex(segment.x, segment.y)
 			}
 		}
 	}
@@ -154,18 +165,71 @@ function intersectSegment(a, b, c, d) {
 	);
 }
 
+function getRayIntersects(polygon, points) {
+	const intersects = [];
 
-function findIntersect(p, guard, first, last) {
-	for(let i = first; (first > last && i < p.polygon.length) || i < last; i = (i+1) % p.polygon.length) {
-		if(i !== first && intersectRay(guard, p.polygon[first], p.polygon[i], p.polygon[(i+1) % p.polygon.length])) {
-			return [calculateIntersect(guard, p.polygon[first], p.polygon[i], p.polygon[(i+1) % p.polygon.length]), p.polygon[first]]
-		}
-		if((i+1) % p.polygon.length !== last && intersectRay(guard, p.polygon[last], p.polygon[i], p.polygon[(i+1) % p.polygon.length])) {
-			return [calculateIntersect(guard, p.polygon[last], p.polygon[i], p.polygon[(i+1) % p.polygon.length]), p.polygon[last]]
-		}
+	for (let i = 0; i < polygon.length; i++) {
+		if (intersectRay(points[0], points[1], polygon[i], polygon[i === polygon.length - 1 ? 0 : i + 1]))
+			intersects.push([polygon[i], polygon[i === polygon.length - 1 ? 0 : i + 1]]);
 	}
+	return intersects;
 }
 
+function findClosestIntersects(p, guard, first, last) {
+	const leftIntersects = []
+	const rightIntersects = []
+	// Find all intersects of polygon edges with the ray from the guard to the polygon vertex
+	for(let i = first; (first > last && i >= first && i < p.polygon.length) || i < last; i = (i+1) % p.polygon.length) {
+		if(i !== first && intersectRay(guard, p.polygon[first], p.polygon[i], p.polygon[(i+1) % p.polygon.length])) {
+			leftIntersects.push([calculateIntersect(guard, p.polygon[first], p.polygon[i], p.polygon[(i + 1) % p.polygon.length]), p.polygon[first]])
+		}
+		if((i+1) % p.polygon.length !== last && intersectRay(guard, p.polygon[last], p.polygon[i], p.polygon[(i+1) % p.polygon.length])) {
+			rightIntersects.push([calculateIntersect(guard, p.polygon[last], p.polygon[i], p.polygon[(i + 1) % p.polygon.length]), p.polygon[last]])
+		}
+	}
+
+	// Find the closest point to the polygon vertex
+	let bestLeft
+	if(leftIntersects.length > 0) {
+		bestLeft = leftIntersects[0]
+		for(let challenger of leftIntersects) {
+			if((challenger[0].x-challenger[1].x)**2 + (challenger[0].y-challenger[1].y)**2 < (bestLeft[0].x-bestLeft[1].x)**2 + (bestLeft[0].y-bestLeft[1].y)**2) {
+				bestLeft = challenger
+			}
+		}
+	}
+
+	let bestRight
+	if(rightIntersects.length > 0) {
+		bestRight = rightIntersects[0]
+		for(let challenger of rightIntersects) {
+			if((challenger[0].x-challenger[1].x)**2 + (challenger[0].y-challenger[1].y)**2 < (bestRight[0].x-bestRight[1].x)**2 + (bestRight[0].y-bestRight[1].y)**2) {
+				bestRight = challenger
+			}
+		}
+	}
+
+	// Remove the ones passing outside the polygon
+	if(bestLeft !== undefined && getRayIntersects(p.polygon, [new Point((p.polygon[first].x + bestLeft[0].x)/2, (p.polygon[first].y + bestLeft[0].y)/2), farPoint]).length % 2 === 0) {
+		bestLeft = undefined
+	}
+	if(bestRight !== undefined && getRayIntersects(p.polygon, [new Point((p.polygon[last].x + bestRight[0].x)/2, (p.polygon[last].y + bestRight[0].y)/2), farPoint]).length % 2 === 0) {
+		bestRight = undefined
+	}
+
+	if(bestLeft !== undefined && bestRight !== undefined) {
+		return [bestLeft[0], bestRight[0]]
+	}
+	else if(bestLeft !== undefined) {
+		return [bestLeft[0]]
+	}
+	else if(bestRight !== undefined) {
+		return [bestRight[0]]
+	}
+	else {
+		return []
+	}
+}
 
 function calculateIntersect(a, b, c, d) {
 	const dx1 = b.x - a.x
@@ -201,4 +265,27 @@ function calculateIntersect(a, b, c, d) {
 	}
 
 	return new Point(x, y)
+}
+
+function checkSimplePolygon(point, points) {
+	// checks if the new point can be added (no intersections with existing edges)
+	if (points.length >= 2) {
+		for (let i = 0; i < points.length - 2; i++) {
+			if (intersectSegment(points[i],	points[i + 1], points[points.length - 1], point)) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+function jiggleCloser(p1, p2) {
+	const d = 0.5
+	const xSign = Math.sign(p2.x - p1.x)
+	const ySign = Math.sign(p2.y - p1.y)
+
+	return [
+		new Point(p1.x + xSign*d, p1.y + ySign*d),
+		new Point(p2.x - xSign*d, p2.y - ySign*d)
+	]
 }
--- sketches.js
+++ sketches.js
@@ -32,14 +32,51 @@ const starOptimal = p => {
 const interactive = p => {
 
 	p.setup = function() {
-		commonSetup(p, "interactive")
+		const canvas = commonSetup(p, "interactive")
+		canvas.mouseClicked(p.click)
+		// p.importPolygon()
 	};
 
 	p.draw = function() {
 		commonDraw(p)
 	};
+
+	p.click = function(event) {
+		const point = new Point(event.layerX, event.layerY)
+		if(p.polygon.length === 0) {
+			if(p.points[0] && Math.sqrt((point.x-p.points[0].x)**2 + (point.y-p.points[0].y)**2) < 6) {
+				p.closePolygon()
+			}
+			else if(checkSimplePolygon(point, p.points)) {
+				p.points.push(point);
+			}
+		}
+		else if(getRayIntersects(p.polygon, [point, farPoint]).length % 2 !== 0) {
+			p.guards.push(point)
+		}
+	}
+
+	p.clearAll = function() {
+		reset(p)
+	}
+
+	p.clearGuards = function() {
+		resetGuards(p)
+	}
+
+	p.closePolygon = function() {
+		if (p.polygon.length === 0 && p.points.length > 2 && checkSimplePolygon(p.points[0], p.points.slice(1))) {
+			p.polygon = p.points.slice();
+			p.points = []
+		}
+	}
+
+	p.importPolygon = function() {
+		reset(p)
+		p.polygon = savedPolygon
+	}
 };
 
 new p5(starBad)
 new p5(starOptimal)
-new p5(interactive)
+const interactiveObject = new p5(interactive)
